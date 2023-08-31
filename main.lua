b167ab39473b92c9c046eac98b47ff3633c77220
Robin Vyslouzil <vyslouzil@shoptet.cz>
2023-08-31 15:41:52 +0200

    Add TOC file

--- main.lua
+++ main.lua
@@ -1,17 +1,20 @@
 -- this is a world of warcraft addon, therefore it uses the wow api
 
 function BohemianV2:OnEnable()
-    -- Called when the addon is enabled
+  -- Called when the addon is enabled
 end
 
 function BohemianV2:OnDisable()
-    -- Called when the addon is disabled
+  -- Called when the addon is disabled
 end
 
 -- when the addon is first loaded open settings
 function BohemianV2:OnInitialize()
-    self:Print("BohemianV2 loaded")
-    self:OpenSettings()
+  self:Print("BohemianV2 loaded")
+  self:OpenSettings()
 end
 
 -- open the settings window
+function BohemianV2:OpenSettings()
+-- create the settings window
+end
