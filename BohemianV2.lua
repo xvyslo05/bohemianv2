eff20fd8ce67f3c3350a8ce3a7b6d462e52f1aab
Robin Vyslouzil <vyslouzil@shoptet.cz>
2023-08-31 15:56:47 +0200

    Add print

--- BohemianV2.lua
+++ BohemianV2.lua
@@ -1,20 +1,2 @@
--- this is a world of warcraft addon, therefore it uses the wow api
+print('hello world' .. UnitName("player") .. ' from addon');
 
-function BohemianV2:OnEnable()
-  -- Called when the addon is enabled
-end
-
-function BohemianV2:OnDisable()
-  -- Called when the addon is disabled
-end
-
--- when the addon is first loaded open settings
-function BohemianV2:OnInitialize()
-  self:Print("BohemianV2 loaded")
-  self:OpenSettings()
-end
-
--- open the settings window
-function BohemianV2:OpenSettings()
--- create the settings window
-end
