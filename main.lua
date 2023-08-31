c5d59afbfe6c52cc655b52eb79367c5e315ad69e
Robin Vyslouzil <vyslouzil@shoptet.cz>
2023-08-31 15:20:09 +0200

    Add main file

---
+++ main.lua
@@ -0,0 +1,17 @@
+-- this is a world of warcraft addon, therefore it uses the wow api
+
+function BohemianV2:OnEnable()
+    -- Called when the addon is enabled
+end
+
+function BohemianV2:OnDisable()
+    -- Called when the addon is disabled
+end
+
+-- when the addon is first loaded open settings
+function BohemianV2:OnInitialize()
+    self:Print("BohemianV2 loaded")
+    self:OpenSettings()
+end
+
+-- open the settings window
